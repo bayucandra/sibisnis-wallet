9c322c9dcb1c35054e9e9b745aa11deb6b57201f
Shahzaib Shahid <shahzaib.shahid912@gmail.com>
2018-06-22 18:45:02 +0500

    Implemented profile image menu along with drag and drop feature

--- src/components/DashboardLayout/Profile/ProfileImagePreview/ProfileImagePreview.jsx
+++ src/components/DashboardLayout/Profile/ProfileImagePreview/ProfileImagePreview.jsx
@@ -6,7 +6,9 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import rightArrowIcon from './../../../../images/icons/s-litle-right.svg'
-
+import backBlueIcon from './../../../../images/icons/ico-back-blue.svg'
+import {modalToggle} from './../../../../lib/utilities';
+import {modalTypes} from './../../../../lib/constants';
 import './ProfileImagePreview.css';
 
 class ProfileImagePreview extends Component {
@@ -14,6 +16,7 @@ class ProfileImagePreview extends Component {
     super(props);
     this.state = {
       anchorEl: null,
+      menuExpand: false
     }
   }
 
@@ -22,14 +25,22 @@ class ProfileImagePreview extends Component {
   }
 
   onMenuClose = () => {
-    this.setState({ anchorEl: null });
+    this.setState({ anchorEl: null, menuExpand:false });
   };
+
+  onExpandMenu = (status) => {
+    this.setState({ menuExpand: status })
+  }
+
+  getImageFromGallery = () => {
+    modalToggle.next({ status: true, type: modalTypes.imageUpload })
+  }
 
   render() {
     const { anchorEl } = this.state;
     return (
-      <div className="profile-image-preview-container" onClick={this.onMenuClose.bind(this)}>
-        <img src={`..${this.props.data.image}`} alt="" className="image-preview" />
+      <div className="profile-image-preview-container" >
+        <img src={`..${this.props.data.image}`} alt="" className="image-preview" onClick={this.onMenuClose.bind(this)} />
         <div className="profile-menu-container">
           <div className="profile-menu-button ripple icon-touch-area-container-40">
             {/* <img src={menuIcon} alt="menu-icon" /> */}
@@ -42,28 +53,39 @@ class ProfileImagePreview extends Component {
               <img src={menuIcon}
                 className="menu-btn ripple" />
             </IconButton>
-            {/* <Menu
-              id="simple-menu"
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={this.onMenuClose.bind(this)}
-              className="profile-menu-item-container"
-            >
-              <MenuItem className="profile-menu">
-                <div className="profile-menu-item"><span className="profile-menu-item__text">Ganti Gambar</span><img src={rightArrowIcon} alt="" /></div>
-              </MenuItem>
-              <Divider></Divider>
-              <MenuItem className="profile-menu">
-              <div className="profile-menu-item"><span className="profile-menu-item__text">Tutup</span></div>
-              </MenuItem>
-            </Menu> */}
           </div>
         </div>
         {this.state.anchorEl ?
-              <div className="custom-menu">
-                <div className="custom-menu-item">Ganti Gambar</div>
-                <div className="custom-menu-item">Tutup</div>
-              </div> : ''}
+          <div className="custom-menu">
+            {!this.state.menuExpand ?
+              <div>
+                <div className="custom-menu-item ripple" onClick={this.onExpandMenu.bind(this,true)}>
+                  <div className="custom-menu-item__text">
+                    <span>Ganti Gambar</span>
+                    <img src={rightArrowIcon} alt="" />
+                  </div>
+                </div>
+                <div className="custom-menu-item ripple" onClick={this.onMenuClose.bind(this)}>
+                  <div className="custom-menu-item__text">Tutup</div>
+                </div>
+              </div>
+              :
+              <div>
+                <div className="custom-menu-item expand ripple" onClick={this.onExpandMenu.bind(this,false)}>
+                  <div className="custom-menu-item__text">
+                    <img src={backBlueIcon} alt="" />
+                    <span className="active">Ganti Gambar</span>
+                  </div>
+                </div>
+                <div className="custom-menu-item expand ripple" onClick={this.getImageFromGallery.bind(this)}>
+                  <div className="custom-menu-item__text">Ambil dari galeri</div>
+                </div>
+                <div className="custom-menu-item expand ripple">
+                  <div className="custom-menu-item__text">Ambil dari kamera</div>
+                </div>
+              </div>
+            }
+          </div> : ''}
       </div>
     )
   }

e7bf233b767a0abc2af96916eac0cc21a8f43ee9
Shahzaib Shahid <shahzaib.shahid912@gmail.com>
2018-06-27 21:29:50 +0500

    Implemented webcam capture confirm and reset feature while taking picture from webcam

--- src/components/Shared/WebcamCapture/WebcamCapture.jsx
+++ src/components/Shared/WebcamCapture/WebcamCapture.jsx
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import Webcam from 'react-webcam';
 import closeIconBlack from './../../../images/icons/ico-close-black.svg';
 import cameraIconWhite from './../../../images/icons/ico-camera-white.svg';
+import cameraFinishIcon from './../../../images/icons/ico-camera-finish.svg';
+import cameraIconReset from './../../../images/icons/ico-upload-reset.svg';
+
 import './WebcamCapture.css';
 import { modalToggle } from '../../../lib/utilities';
 
+const CameraResetButton = (props) => {
+  const { onCameraReset } = props;
+  return (
+    <div className="camera-reset-button-container">
+      <div className="camera-reset-button ripple" onClick={onCameraReset}>
+        <img src={cameraIconReset} className="camera-reset-button__icon" alt="camera-reset-icon" />
+        <span className="camera-reset-button__text">Ambil ulang</span>
+      </div>
+    </div>
+  )
+}
 class WebcamCapture extends Component {
   constructor(props) {
     super(props);
@@ -35,8 +49,12 @@ class WebcamCapture extends Component {
     modalToggle.next({ status: false });
   }
 
+  onCameraReset = () => {
+    this.setState({ captureImage: null, captured: false });
+  }
+
   render() {
-    const { captured,captureImage} = this.state;
+    const { captured, captureImage } = this.state;
     return (
       <div className="webcam-capture-container">
         <div className="webcam-header">
@@ -57,16 +75,20 @@ class WebcamCapture extends Component {
         </div>
 
 
-        <div className="capture-button-container">
+        <div className={"capture-button-container " + (captured ? 'capture-button-finish-container' : '')}>
           {!captured ?
             <div className="capture-button ripple" onClick={this.capture.bind(this)}>
               <img src={cameraIconWhite} alt="camera-icon" />
             </div>
             :
-            <div className="capture-button ripple" onClick={this.onCaptureConfirm.bind(this)}>
-              OK
+            <div className="capture-button capture-button-finish ripple" onClick={this.onCaptureConfirm.bind(this)}>
+              <img src={cameraFinishIcon} alt="camera-icon-finish" />
             </div>}
         </div>
+
+        {captured ?
+          <CameraResetButton onCameraReset={this.onCameraReset.bind(this)} />
+          : null}
       </div>)
   }
 }

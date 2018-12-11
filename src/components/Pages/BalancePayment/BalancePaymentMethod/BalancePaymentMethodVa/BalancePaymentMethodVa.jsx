d4f64bd8871fdfcde5a9febb5b6aabfc6505b64d
Bayu Candra <bayu@biqdev.com>
2018-12-11 14:00:39 +0700

    Prepare for VA

---
+++ src/components/Pages/BalancePayment/BalancePaymentMethod/BalancePaymentMethodVa/BalancePaymentMethodVa.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import "./BalancePaymentMethodVa.scss";
+
+export default class BalancePaymentMethodVa extends Component {
+
+  render() {
+
+
+
+  }//render()
+
+}
